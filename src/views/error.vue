1964390eaae405f628a394fc3e1ba19b2b8c3448
yangdupeng@cftig.com <yangdupeng@cftig.com>
2018-09-04 13:44:39 +0800

    update init.css

---
+++ src/views/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="error">
+        404
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "error"
+    }
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus">
+ .error
+    font-size:20px;
+    background:red;
+</style>
